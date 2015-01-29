c795da55edd466ef71e33e40540d5967e9283b58
Matthias Klapperstueck <m.klapperstueck@gmail.com>
2015-01-29 13:29:36 +1100

    created proper compile pipeline

--- createfilelistWin.cmd
+++ createfilelistWin.cmd
@@ -2,7 +2,7 @@
 cd /d %~dp0
 echo "Create XML plugin file lists..."
 echo Vanted Plugins...
-dir src\main\java\plugin.xml /s /b > src\main\resources\plugins.txt 2> nul
+dir src\main\java\*.xml /s /b > src\main\resources\plugins.txt 2> nul
 
 
 echo create Cluster Plugin List
