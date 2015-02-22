da72171eb8842d54d10967be27dbe34b281d80df
Matthias Klapperstueck <m.klapperstueck@gmail.com>
2015-02-23 09:34:32 +1100

    bugfix: now the listelement is highlighted when selected

--- src/main/java/de/ipk_gatersleben/ag_nw/graffiti/services/algorithms/AlgorithmListCellRenderer.java
+++ src/main/java/de/ipk_gatersleben/ag_nw/graffiti/services/algorithms/AlgorithmListCellRenderer.java
@@ -6,6 +6,7 @@ package de.ipk_gatersleben.ag_nw.graffiti.services.algorithms;
 import java.awt.Color;
 import java.awt.Component;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
@@ -19,15 +20,18 @@ import org.graffiti.plugin.algorithm.Algorithm;
 public class AlgorithmListCellRenderer 
 extends JLabel implements ListCellRenderer<Algorithm>{
 
-	private static final Color selectedColor = new Color(200,230,200);
+	private static final Color selectedColor = new Color(250,250,150);
 
-	private static final Color focusColor = new Color(230,230,0);
+
+	private static final Color defaultColor = new Color(255,255,255);
 
 	/**
 	 * 
 	 */
 	public AlgorithmListCellRenderer() {
 		setIconTextGap(12);
+		setOpaque(true);
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 	}
 	
 	
@@ -35,12 +39,9 @@ extends JLabel implements ListCellRenderer<Algorithm>{
 	public Component getListCellRendererComponent(
 			JList<? extends Algorithm> list, Algorithm value, int index,
 			boolean isSelected, boolean cellHasFocus) {
-		
+		setBackground(defaultColor);
 		if(isSelected) {
 			setBackground(selectedColor);
-		}
-		if(cellHasFocus) {
-			setBackground(focusColor);
 		}
 		
 		setText(value.getName());	
