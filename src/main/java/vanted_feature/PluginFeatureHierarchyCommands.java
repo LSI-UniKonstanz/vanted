55dc2801d3eab4defed9895c541a4dfa495c3ade
Matthias Klapperstueck <m.klapperstueck@gmail.com>
2015-05-05 18:20:53 +1000

    added comments on what algorithms related to hierarchies to keep and which to merge and which to remove

--- src/main/java/vanted_feature/PluginFeatureHierarchyCommands.java
+++ src/main/java/vanted_feature/PluginFeatureHierarchyCommands.java
@@ -11,6 +11,9 @@ import org.SettingsHelperDefaultIsTrue;
 import org.graffiti.plugin.algorithm.Algorithm;
 
 import de.ipk_gatersleben.ag_nw.graffiti.IPK_PluginAdapter;
+import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.dbe.CreateFuncatGraphAlgorithm;
+import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.dbe.database_processing.go.CreateGOtreeAlgorithm;
+import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.dbe.database_processing.go.InterpreteGOtermsAlgorithm;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.dbe.database_processing.go_cluster_histogram.CreateDirectChildrenClustersHistogramAlgorithm;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.dbe.database_processing.go_cluster_histogram.ProcessHierarchynodesDepOnLeafNodes;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.dbe.database_processing.go_cluster_histogram.PruneTreeAlgorithm;
@@ -34,14 +37,36 @@ extends IPK_PluginAdapter {
 		if (new SettingsHelperDefaultIsTrue().isEnabled("Hierarchy commands")) {
 			if (ReleaseInfo.getRunningReleaseStatus() != Release.KGML_EDITOR) {
 				this.algorithms = new Algorithm[] {
-						//									new HierarchyAlgorithms(),
-						new CreateDirectChildrenClustersHistogramAlgorithm(),
-						new PruneTreeAlgorithm(),
-						new HierarchyWizard(),
-						new HideOrShowChildNodes(),
-						new CreateHierarchyTree(),
-						new ProcessHierarchynodesDepOnLeafNodes(),
-
+						//									new HierarchyAlgorithms(), //Old housing of the algorithms
+						
+						/*
+						 * some of the following algorithms will be removed from the final version of vanted
+						 * since they seem to be very implemented for very special use
+						 * 
+						 * The latest vanted version will focus on GO/KEGG hierarchy including 
+						 * cluster histograms
+						 * 
+						 * All disabled algorithms will be in a special category (See category list //TODO: to be implemented).
+						 * If the user enables a global setting e.g. enable special/extra algorithms. they will reappear
+						 * 
+						 * So.. the algorithms will be listed in the global algorithm list but will not be added to the menu
+						 * but will be available in the vanted classpath, to be called by other algorithms if needed
+						 */
+						new CreateDirectChildrenClustersHistogramAlgorithm(), // Disable for normal use
+						new PruneTreeAlgorithm(), //Disable for normal use
+						new HierarchyWizard(), // Disable for normal use, since its functionality doesn't match what we want
+						new HideOrShowChildNodes(), //Disable for normal use. We don't need that now
+						new CreateHierarchyTree(), //Another Super-Dialog containing hierarchy algorithms
+							//including:	CreateGOtreeAlgorithm(), 		//Disable for normal use 
+							//				InterpreteGOtermsAlgorithm(), 	//combine the two into one
+							//				CreateFuncatGraphAlgorithm()
+							/*
+							 *  The two latter algos will be combined and the resulting algorithm will support
+							 *  GO and KEGG and EC and maybe later other hierarchy searching and creation algorithms
+							 *  	think of a new interface with easy extension
+							 */
+						new ProcessHierarchynodesDepOnLeafNodes(), //Super-dialog that contains the actutal algorithms to create the hierarchies
+							//including: 	
 
 						new CreateKeggReactionNetworkAlgorithm(),
 						new HierarchyAlgorithm(),
