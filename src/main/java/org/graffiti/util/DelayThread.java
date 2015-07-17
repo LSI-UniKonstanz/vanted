980fb23a8ae250ea617331016a05df6a69f636e7
Matthias Klapperstueck <m.klapperstueck@gmail.com>
2015-07-17 15:25:27 +1000

    logger is static variable now code formatting

--- src/main/java/org/graffiti/util/DelayThread.java
+++ src/main/java/org/graffiti/util/DelayThread.java
@@ -11,74 +11,77 @@ import org.graffiti.event.AttributeEvent;
 
 /**
  * @author matthiak
- *
  */
 /**
  * This delay thread will help prevent too many calls in short time
  * by catching the calls and only giving the last stored event after
  * a short period of time to the actual event handler using a
  * callback mechanism
- * 
  * If it has delivered the event it will trigger "wait" and halt the thread
  * A new event will then wake up the thread.
+ * 
  * @author matthiak
- *
  */
 public class DelayThread extends Thread {
-	private Logger logger = Logger.getLogger(DelayThread.class);
-
+	private static Logger logger = Logger.getLogger(DelayThread.class);
+	
+	static {
+		logger.setLevel(Level.INFO);
+	}
 	static final int MAX_COUNT = 10;
 	int counter;
 	DelayedCallback callback;
 	AttributeEvent e;
+	
 	/**
 	 * 
 	 */
 	public DelayThread(DelayedCallback callback) {
 		this.callback = callback;
-		logger.setLevel(Level.INFO);
+		
 	}
+	
 	@Override
 	public void run() {
-		while(true){
+		while (true) {
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			increment();
-			if(counter > MAX_COUNT){
+			if (counter > MAX_COUNT) {
 				SwingUtilities.invokeLater(new Runnable() {
-
+					
 					@Override
 					public void run() {
 						logger.debug("invoking callback");
 						callback.call(e);
 					}
 				});
-
+				
 				hibernate();
 			}
 		}
 	}
-
+	
 	public synchronized void setAttributeEvent(AttributeEvent e) {
 		logger.debug("setting attribute");
 		notify();
 		this.e = e;
 		reset();
-	}		
-
+	}
+	
 	private void reset() {
 		logger.debug("resetting counter");
 		counter = 0;
 	}
-
+	
 	private void increment() {
 		logger.debug("incrementing");
 		counter++;
 	}
-
+	
 	private synchronized void hibernate() {
 		logger.debug("going to hibernate");
 		try {
@@ -88,13 +91,12 @@ public class DelayThread extends Thread {
 		}
 		logger.debug("got wakeup call");
 	}
-
-
+	
 	/**
 	 * This callback interface is used by the DelayThread
 	 * Implementing classes can set the method to be called
+	 * 
 	 * @author matthiak
-	 *
 	 */
 	public interface DelayedCallback {
 		public void call(AttributeEvent e);
