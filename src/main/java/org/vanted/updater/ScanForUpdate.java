a524505b1309564e93e9661cd4ca6f1787b0ec63
Matthias Klapperstueck <m.klapperstueck@gmail.com>
2015-08-25 13:57:33 +1000

    further development on update mechanism

--- src/main/java/org/vanted/updater/ScanForUpdate.java
+++ src/main/java/org/vanted/updater/ScanForUpdate.java
@@ -6,6 +6,7 @@ package org.vanted.updater;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,12 +21,17 @@ import java.util.prefs.Preferences;
 
 import javax.swing.JOptionPane;
 
+import org.ApplicationStatus;
+import org.ErrorMsg;
 import org.ReleaseInfo;
 import org.apache.commons.lang.time.DateUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.graffiti.editor.MainFrame;
 import org.graffiti.managers.PreferenceManager;
+import org.graffiti.options.PreferencesInterface;
+import org.graffiti.plugin.parameter.Parameter;
+import org.graffiti.plugin.parameter.StringParameter;
 
 import de.ipk_gatersleben.ag_nw.graffiti.FileHelper;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.helper.DBEgravistoHelper;
@@ -37,21 +43,21 @@ import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.helper.DBEgravistoHelper;
  *         # comment line
  *         version: 2.5.0
  *         core: vanted-core.jar
- *         core-lib: lib-name.jar
+ *         lib: lib-name.jar
  *         // # end of update entry
  */
-public class ScanForUpdate {
+public class ScanForUpdate implements PreferencesInterface {
+	
+	private static final Logger logger = Logger.getLogger(ScanForUpdate.class);
 	
 	/**
 	 * 
 	 */
 	private static final int TIME_REMINDER_DAYS = 7;
 	
-	private static final Logger logger = Logger.getLogger(ScanForUpdate.class);
-	
 	private static final String[] arrDialogOptions = {
-			"Update now",
-			"Remind me in 7 days"
+			"Download now",
+			"Remind me in " + TIME_REMINDER_DAYS + " days"
 	};
 	
 	/**
@@ -63,17 +69,41 @@ public class ScanForUpdate {
 	 */
 	private static final String VERSIONSTRING = "version";
 	private static final String CORESTRING = "core";
-	private static final String CORELIBSTRING = "core-lib";
+	private static final String LIBSTRING = "lib";
 	
-	private static final String urlUpdateBaseString = "https://immersive-analytics.infotech.monash.edu/vanted-new2.5/release/updates/";
-	private static final String urlUpdateFileString = urlUpdateBaseString + "vanted-update";
+	private static String URL_UPDATE_BASESTRING = "https://immersive-analytics.infotech.monash.edu/vanted-new2.5/release/updates/";
+	private static String URL_UPDATE_FILESTRING = URL_UPDATE_BASESTRING + "vanted-update";
 	
-	private static final String destPathUpdateDir = ReleaseInfo.getAppFolderWithFinalSep() + "update/";
-	private static final String destUpdateFile = ReleaseInfo.getAppFolderWithFinalSep() + "do-vanted-update";
-	private static final String vantedUpdateOKFile = ReleaseInfo.getAppFolderWithFinalSep() + "vanted-update-ok";
+	private static final String DESTPATHUPDATEDIR = ReleaseInfo.getAppFolderWithFinalSep() + "update/";
+	private static final String DESTUPDATEFILE = DESTPATHUPDATEDIR + "do-vanted-update";
+	private static final String VANTEDUPDATEOKFILE = DESTPATHUPDATEDIR + "vanted-update-ok";
+	
+	public static void scanAfterStartup() {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				logger.debug("waiting for update scan until app has started");
+				while (ErrorMsg.getAppLoadingStatus() != ApplicationStatus.ADDONS_LOADED) {
+					try {
+						Thread.sleep(1000);//sleep 20 sec
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					doScan();
+				} catch (IOException e) {
+					if (Logger.getRootLogger().getLevel() == Level.DEBUG)
+						e.printStackTrace();
+					System.out.println("cannot scan for updates: " + e.getMessage());
+				}
+			}
+		}).start();
+	}
 	
 	public static void doScan() throws IOException {
-		
+		logger.debug("doing update scan at: " + URL_UPDATE_FILESTRING);
 		cleanPreviousUpdate();
 		
 		Date currentDate = new Date();
@@ -84,9 +114,18 @@ public class ScanForUpdate {
 		String strCoreProgramRelativePath = null;
 		List<String> listStrCoreLibsRelativePaths = new ArrayList<String>();
 		
-		URL updateURL = new URL(urlUpdateFileString);
-		InputStream openStream = updateURL.openStream();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(openStream));
+		InputStream inputstreamURL = null;
+		
+		URL updateURL = new URL(URL_UPDATE_FILESTRING);
+		
+		try {
+			inputstreamURL = updateURL.openStream();
+		} catch (FileNotFoundException e1) {
+			System.out.println("no updates available");
+			return;
+		}
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputstreamURL));
 		String line;
 		while ((line = reader.readLine()) != null) {
 			if (line.equals("//"))
@@ -94,19 +133,21 @@ public class ScanForUpdate {
 			if (line.startsWith("#"))
 				continue;
 			if (line.toLowerCase().startsWith(VERSIONSTRING)) {
-				version = line.substring(VERSIONSTRING.length() + 2).trim();
+				version = line.substring(VERSIONSTRING.length() + 1).trim();
 				prepareUpdate = updateIsNewer(version);
 			}
 			if (line.toLowerCase().startsWith(CORESTRING)) {
-				strCoreProgramRelativePath = line.substring(CORESTRING.length() + 2).trim();
+				strCoreProgramRelativePath = line.substring(CORESTRING.length() + 1).trim();
 			}
-			if (line.toLowerCase().startsWith(CORELIBSTRING)) {
-				listStrCoreLibsRelativePaths.add(line.substring(CORELIBSTRING.length() + 2).trim());
+			if (line.toLowerCase().startsWith(LIBSTRING)) {
+				listStrCoreLibsRelativePaths.add(line.substring(LIBSTRING.length() + 1).trim());
 			}
 			
 		}
 		
-		if (logger.getLevel() == Level.DEBUG) {
+		inputstreamURL.close();
+		
+		if (Logger.getRootLogger().getLevel() == Level.DEBUG) {
 			logger.debug("We found an update.. printing locations:");
 			System.out.println("Core: " + strCoreProgramRelativePath);
 			for (String libPath : listStrCoreLibsRelativePaths) {
@@ -130,7 +171,7 @@ public class ScanForUpdate {
 				if (currentDate.after(storedDate))
 					timeout = true;
 			} catch (ParseException e) {
-				if (logger.getLevel() == Level.DEBUG)
+				if (Logger.getRootLogger().getLevel() == Level.DEBUG)
 					e.printStackTrace();
 				timeout = true;
 			}
@@ -146,8 +187,8 @@ public class ScanForUpdate {
 						+ "You can download it now or be reminded later.<br/><br/>"
 						+ "The update will be installed during the next startup.",
 				"Update available",
+				JOptionPane.OK_CANCEL_OPTION,
 				JOptionPane.QUESTION_MESSAGE,
-				JOptionPane.OK_OPTION,
 				null,
 				arrDialogOptions,
 				arrDialogOptions[0]);
@@ -162,32 +203,29 @@ public class ScanForUpdate {
 		
 		// if ok.. download files
 		// create directories and 
-		File updateDir = new File(destPathUpdateDir);
+		File updateDir = new File(DESTPATHUPDATEDIR);
 		if (!updateDir.exists())
 			updateDir.mkdirs();
-		else {
-			logger.error("directory should never previously exist.. bootstrapupdater failed");
-		}
 		
 		// create a copy file for the bootstrap program
 		// that will put the files to the right place
 		// .. the next thing is for the bootstrap
 		
-		File updateFile = new File(destUpdateFile);
+		File updateFile = new File(DESTUPDATEFILE);
 		BufferedWriter writer = new BufferedWriter(new FileWriter(updateFile));
 		
 		writer.write(VERSIONSTRING + ":" + version);
 		writer.newLine();
 		if (strCoreProgramRelativePath != null) {
-			FileHelper.downloadFile(new URL(urlUpdateBaseString + strCoreProgramRelativePath), destPathUpdateDir, extractFileName(strCoreProgramRelativePath));
+			FileHelper.downloadFile(new URL(URL_UPDATE_BASESTRING + strCoreProgramRelativePath), DESTPATHUPDATEDIR, extractFileName(strCoreProgramRelativePath));
 			writer.write(CORESTRING + ":" + strCoreProgramRelativePath);
 			writer.newLine();
 		}
 		
 		for (String libPath : listStrCoreLibsRelativePaths) {
 			
-			FileHelper.downloadFile(new URL(urlUpdateBaseString + libPath), destPathUpdateDir, extractFileName(libPath));
-			writer.write(CORESTRING + ":" + libPath);
+			FileHelper.downloadFile(new URL(URL_UPDATE_BASESTRING + libPath), DESTPATHUPDATEDIR, extractFileName(libPath));
+			writer.write(LIBSTRING + ":" + libPath);
 			writer.newLine();
 		}
 		
@@ -206,15 +244,16 @@ public class ScanForUpdate {
 	 * This will check, if the bootstrap was successfully
 	 * updating vanted by checking an OK file and then
 	 * delete the update folder
+	 * And by deleting the update folder (including this file) we also tell the bootstrap
+	 * that we successfully loaded
 	 */
 	private static void cleanPreviousUpdate() {
-		File checkfile = new File(vantedUpdateOKFile);
+		File checkfile = new File(VANTEDUPDATEOKFILE);
 		if (checkfile.exists())
-			FileHelper.deleteDirRecursively(new File(destPathUpdateDir));
+			FileHelper.deleteDirRecursively(new File(DESTPATHUPDATEDIR));
 	}
 	
 	private static boolean updateIsNewer(String remoteVersion) {
-		boolean isNewer = false;
 		
 		String[] strSplitMainversion = DBEgravistoHelper.DBE_GRAVISTO_VERSION_CODE.split("\\.");
 		
@@ -237,12 +276,32 @@ public class ScanForUpdate {
 			 * we don't need to check further.
 			 */
 			if (longPartVersion > longArrMainVersion[i]) {
-				return isNewer;
+				return true;
 				
 			}
 			
 		}
 		
-		return isNewer;
+		return false;
 	}
+	
+	@Override
+	public List<Parameter> getDefaultParameters() {
+		List<Parameter> params = new ArrayList<Parameter>();
+		params.add(new StringParameter(URL_UPDATE_BASESTRING, "Update URL", "Location of the URL to look for updates"));
+		return params;
+	}
+	
+	@Override
+	public void updatePreferences(Preferences preferences) {
+		URL_UPDATE_BASESTRING = preferences.get("Update URL", URL_UPDATE_BASESTRING);
+		
+		URL_UPDATE_FILESTRING = URL_UPDATE_BASESTRING + "vanted-update";
+	}
+	
+	@Override
+	public String getPreferencesAlternativeName() {
+		return "Update";
+	}
+	
 }
