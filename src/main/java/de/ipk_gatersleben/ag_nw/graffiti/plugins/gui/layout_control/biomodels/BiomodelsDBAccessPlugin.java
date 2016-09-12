a18b99fe9d9ca0c38f237f7fd2a65a8849abfde9
Matthias Klapperstueck <matthias.klapperstueck@monash.edu>
2016-09-12 15:33:24 +1000

    -added BioModels preference to change webservice endpoint to custom location -Biomodels connector checks automatically the known backup location if primary webservice connector is not available

--- src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/gui/layout_control/biomodels/BiomodelsDBAccessPlugin.java
+++ src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/gui/layout_control/biomodels/BiomodelsDBAccessPlugin.java
@@ -8,17 +8,18 @@ import org.graffiti.plugin.inspector.InspectorTab;
 
 /**
  * @author matthiak
- *
  */
-public class BiomodelsDBAccessPlugin extends EditorPluginAdapter{
-
+public class BiomodelsDBAccessPlugin extends EditorPluginAdapter
+{
+	
 	/**
 	 * 
 	 */
 	public BiomodelsDBAccessPlugin() {
-	
+		
 		tabs = new InspectorTab[] {
 				new TabBiomodels()
 		};
 	}
+	
 }
--- src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/gui/layout_control/biomodels/BiomodelsPanel.java
+++ src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/gui/layout_control/biomodels/BiomodelsPanel.java
@@ -7,6 +7,7 @@ import info.clearthought.layout.TableLayout;
 import info.clearthought.layout.TableLayoutConstraints;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -19,6 +20,7 @@ import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -35,16 +37,15 @@ import de.ipk_gatersleben.ag_nw.graffiti.services.task.BackgroundTaskHelper;
 
 /**
  * @author matthiak
- *
  */
 public class BiomodelsPanel extends JPanel
-implements ActionListener, BiomodelsLoaderCallback, KeyListener{
+		implements ActionListener, BiomodelsLoaderCallback, KeyListener {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5772252664151135872L;
-
+	
 	Logger logger = Logger.getLogger(BiomodelsPanel.class);
 	
 	JScrollPane scrollpane;
@@ -70,6 +71,11 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 	CallerThreadForSimpleModel callerThreadForSimpleModel;
 	
 	CallerThreadForSBMLModel callerThreadForSBML;
+	
+	private JLabel labelServiceAvailable;
+	private boolean isServiceAvailable;
+	private String connectivityString;
+	
 	/**
 	 * 
 	 */
@@ -78,22 +84,54 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 		adapter.addListener(this);
 		initGUI();
 //		setPreferredSize(new Dimension(300, 300));
+		checkConnection();
 	}
-
 	
+	/**
+	 * 
+	 */
+	private void checkConnection() {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				try {
+					isServiceAvailable = adapter.isAvailable();
+				} catch (BioModelsWSException e) {
+					e.printStackTrace();
+				}
+				
+				SwingUtilities.invokeLater(new Runnable() {
+					
+					@Override
+					public void run() {
+						
+						if (isServiceAvailable) {
+							connectivityString = "Biomodels DB available";
+							labelServiceAvailable.setForeground(Color.GREEN.darker());
+							labelServiceAvailable.setText(connectivityString);
+						} else {
+							connectivityString = "<html>Biomodels DB not available.<br/><br/>Go to Preferences to change webservice endpoint.";
+							labelServiceAvailable.setForeground(Color.RED.darker());
+							labelServiceAvailable.setText(connectivityString);
+							
+						}
+						
+					}
+				});
+			}
+		}).start();
+	}
 	
 	public BiomodelsAccessAdapter getAdapter() {
 		return adapter;
 	}
-
-
-
+	
 	public void setAdapter(BiomodelsAccessAdapter adapter) {
 		this.adapter = adapter;
 	}
-
-
-
+	
 	/**
 	 * 
 	 */
@@ -106,33 +144,39 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 		
 		scrollpane = new JScrollPane(rootpanel);
 		
-		rootpanel.setLayout(new TableLayout(new double[][]{
+		rootpanel.setLayout(new TableLayout(new double[][] {
 				//columns
 				{
-					5, TableLayout.PREFERRED, 5, TableLayout.FILL, 5, TableLayout.PREFERRED
-				},
+						5, TableLayout.PREFERRED, 5, TableLayout.FILL, 5, TableLayout.PREFERRED
+		},
 				//rows
 				{
-					5, TableLayout.PREFERRED, 
-					5, TableLayout.PREFERRED,
-					5, TableLayout.FILL,
-					5, TableLayout.PREFERRED
-				}
+						5, TableLayout.PREFERRED,
+						5, TableLayout.PREFERRED,
+						5, TableLayout.PREFERRED,
+						5, TableLayout.FILL,
+						5, TableLayout.PREFERRED
+		}
 		}));
+		
+		rootpanel.add(new JLabel("Availability: "), "1,1");
+		labelServiceAvailable = new JLabel("availability");
+		
+		rootpanel.add(labelServiceAvailable, "3,1");
 		
 		BiomodelsAccessAdapter.QueryType[] items = BiomodelsAccessAdapter.QueryType.values();
 		comboQueryType = new JComboBox<BiomodelsAccessAdapter.QueryType>(items);
 		
-		rootpanel.add(comboQueryType, "1,1");
+		rootpanel.add(comboQueryType, "1,3");
 		
 		queryField = new JTextField(20);
 		queryField.addKeyListener(this);
-		rootpanel.add(queryField, "3,1");
+		rootpanel.add(queryField, "3,3");
 		
 		searchButton = new JButton();
 		searchButton.setIcon(new ImageIcon(getClass().getClassLoader().getResource("org/images/lupe.png")));
 		searchButton.addActionListener(this);
-		rootpanel.add(searchButton, "5,1");
+		rootpanel.add(searchButton, "5,3");
 		
 //		constraint = new TableLayoutConstraints(1, 3, 3, 3, TableLayoutConstraints.CENTER, TableLayoutConstraints.CENTER);
 //		progressBar = new JProgressBar();
@@ -149,41 +193,39 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 		
 		loadSelectedModels = new JButton("Load Models");
 		loadSelectedModels.addActionListener(this);
-		rootpanel.add(loadSelectedModels, "1,7");
+		rootpanel.add(loadSelectedModels, "7,3");
 		
 		add(rootpanel, BorderLayout.CENTER);
-		
 		
 		listResults.setEnabled(false);
 		loadSelectedModels.setEnabled(false);
 	}
-
-	private void triggerQuery(String query){
-		BiomodelsAccessAdapter.QueryType selItem = 
-				(BiomodelsAccessAdapter.QueryType)comboQueryType.getSelectedItem();
+	
+	private void triggerQuery(String query) {
+		BiomodelsAccessAdapter.QueryType selItem =
+				(BiomodelsAccessAdapter.QueryType) comboQueryType.getSelectedItem();
 		
-		if(query == null || query.isEmpty())
+		if (query == null || query.isEmpty())
 			return;
-
+		
 //		progressBar.setIndeterminate(true);
 		
-		if(callerThreadForSimpleModel != null && callerThreadForSimpleModel.isAlive())
+		if (callerThreadForSimpleModel != null && callerThreadForSimpleModel.isAlive())
 			callerThreadForSimpleModel.cancelRequest();
-			
-
+		
 		BackgroundTaskHelper.issueSimpleTask(
-				"Biomodels query task", 
-				"query result list", 
-				new CallerThreadForSimpleModel(selItem, query), 
+				"Biomodels query task",
+				"query result list",
+				new CallerThreadForSimpleModel(selItem, query),
 				null);
 //		callerThreadForSimpleModel = new CallerThreadForSimpleModel(selItem, query);
 //		callerThreadForSimpleModel.start();
 	}
-
-	private void triggerLoadSBML(SimpleModel model){
-		if(model == null)
+	
+	private void triggerLoadSBML(SimpleModel model) {
+		if (model == null)
 			return;
-
+		
 //		progressBar.setIndeterminate(true);
 		
 //		if(callerThreadForSBML != null && callerThreadForSBML.isAlive())
@@ -192,11 +234,11 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 //		callerThreadForSBML = new CallerThreadForSBMLModel(model);
 //		callerThreadForSBML.start();
 		BackgroundTaskHelper.issueSimpleTask(
-				"Biomodels query task", 
-				"query result list", 
+				"Biomodels query task",
+				"query result list",
 				new CallerThreadForSBMLModel(model),
 				null);
-
+		
 	}
 	
 	@Override
@@ -205,11 +247,11 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 		
 //		progressBar.setIndeterminate(false);
 		
-		if(simpleModel == null){
+		if (simpleModel == null) {
 			logger.debug("no results");
 			simpleModel = new ArrayList<SimpleModel>();
 		}
-		if(SwingUtilities.isEventDispatchThread())
+		if (SwingUtilities.isEventDispatchThread())
 			listResults.setListData(simpleModel.toArray(new SimpleModel[simpleModel.size()]));
 		else {
 			final List<SimpleModel> simpleModelSwingThread = simpleModel;
@@ -224,7 +266,7 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 		listResults.setEnabled(true);
 		loadSelectedModels.setEnabled(true);
 	}
-
+	
 	@Override
 	public void resultForSBML(SimpleModel model, String modelstring) {
 		logger.debug("having result for SBML");
@@ -233,76 +275,75 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 		listResults.setEnabled(true);
 		loadSelectedModels.setEnabled(true);
 	}
-
+	
 	@Override
 	public void resultError(Exception e) {
 //		progressBar.setIndeterminate(false);
 		
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource().equals(loadSelectedModels)){
+		if (e.getSource().equals(loadSelectedModels)) {
 			
 			List<SimpleModel> selectedValuesList = listResults.getSelectedValuesList();
-			if(selectedValuesList == null || selectedValuesList.isEmpty())
+			if (selectedValuesList == null || selectedValuesList.isEmpty())
 				return;
 			
 			listResults.setEnabled(false);
 			loadSelectedModels.setEnabled(false);
-			for(SimpleModel model : selectedValuesList)
+			for (SimpleModel model : selectedValuesList)
 				triggerLoadSBML(model);
 			
 		}
-		if(e.getSource().equals(searchButton)) {
+		if (e.getSource().equals(searchButton)) {
 			listResults.setEnabled(false);
 			loadSelectedModels.setEnabled(false);
 			triggerQuery(queryField.getText().trim());
 		}
 		
 	}
-
 	
-
 	/*
 	 * for input field
 	 */
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
-
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
 //		logger.debug("keyPressed");
 	}
-
+	
 	@Override
 	public void keyReleased(KeyEvent e) {
 //		logger.debug("keyReleased");
-		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 			listResults.setEnabled(false);
 			loadSelectedModels.setEnabled(false);
 			triggerQuery(queryField.getText().trim());
 		}
 	}
-
+	
 	class ListMouseAdapapter extends MouseAdapter {
-
+		
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			if(e.getClickCount() == 2) {
+			if (e.getClickCount() == 2) {
 				listResults.setEnabled(false);
 				loadSelectedModels.setEnabled(false);
 				triggerLoadSBML(listResults.getSelectedValue());
 			}
-				
+			
 		}
 		
 	}
-
-	class CallerThreadForSimpleModel extends Thread{
+	
+	class CallerThreadForSimpleModel extends Thread {
 		BiomodelsAccessAdapter.QueryType selItem;
 		String query;
+		
 		/**
 		 * 
 		 */
@@ -312,6 +353,7 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 			this.selItem = selItem;
 			this.query = query;
 		}
+		
 		@Override
 		public void run() {
 			try {
@@ -322,13 +364,15 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 				e.printStackTrace();
 			}
 		}
-	
+		
 		public void cancelRequest() {
 			adapter.setAbort(true);
 		}
 	}
-	class CallerThreadForSBMLModel extends Thread{
+	
+	class CallerThreadForSBMLModel extends Thread {
 		SimpleModel model;
+		
 		/**
 		 * 
 		 */
@@ -336,6 +380,7 @@ implements ActionListener, BiomodelsLoaderCallback, KeyListener{
 				SimpleModel model) {
 			this.model = model;
 		}
+		
 		@Override
 		public void run() {
 			try {
