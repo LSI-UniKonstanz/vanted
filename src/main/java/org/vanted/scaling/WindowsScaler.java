1e75855185c5f90ed62ac58c9999b9bfe1cdb742
dim8 <dimitar.garkov@uni-konstanz.de>
2017-04-04 01:55:26 +0200

    windows scaling

--- src/main/java/org/vanted/scaling/WindowsScaler.java
+++ src/main/java/org/vanted/scaling/WindowsScaler.java
@@ -1,53 +1,22 @@
 package org.vanted.scaling;
 
-import java.awt.Font;
-import java.awt.Insets;
-import java.awt.Toolkit;
-
 import javax.swing.Icon;
-import javax.swing.plaf.InsetsUIResource;
 /**
- * Similar to the default scaler, with the only difference that
- * we have to adjust the requested scaling factor to the current.
- * Windows already performs some degree of rescaling of e.g. fonts.
+ * Similar to the default scaler. 
+ * Not necessary/able to scale LAF-Icons.  
  * 
- * [NEEDS FURTHER TESTING: DISABLED FOR NOW!]
  * @author dim8
  *
  */
 public class WindowsScaler extends BasicScaler {
 	
 	public WindowsScaler(float scaleFactor) {
-		super(scaleFactor / getCurrentScaling(scaleFactor));
-	}
-
-	private static float getCurrentScaling(float dpif) {
-		int dpi = Toolkit.getDefaultToolkit().getScreenResolution();
+		super(scaleFactor);
 		
-		return dpi / dpif;
 	}
 
-	//Only this one should (possibly) remain so
 	@Override
 	public Icon modifyIcon(Object key, Icon original) {
 		return original;
-	}
-
-	@Override
-	public Font modifyFont(Object key, Font original) {
-		return original;
-	}
-
-	@Override
-	public Integer modifyInteger(Object key, Integer original) {
-		return original;
-	}
-	
-	@Override
-	public Insets modifyInsets(Insets original, InsetsUIResource original2) {
-		if (original != null)
-			return original;
-		else
-			return original2;
 	}
 }
