9542abb0b85cb14ef0cf1e781e2ed23050665bc4
Dimitar Garkov <dimitar.garkov@uni-konstanz.de>
2018-02-06 20:27:43 +0100

    Vanted Javadoc version-controlled

---
+++ src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/gui/info_dialog_cluster_analysis/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the Info Dialog for IPK-Cluster Analysis.
+ * 
+ * @author Christian Klukas, IPK
+ * @version 1.0.0
+ */
+package de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.info_dialog_cluster_analysis;
