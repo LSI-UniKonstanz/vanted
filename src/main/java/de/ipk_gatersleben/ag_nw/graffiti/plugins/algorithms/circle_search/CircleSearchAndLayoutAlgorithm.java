848b03ac1a3ada6fd3031cc9355b2fcb31631797
Dimitar Garkov <dimitar.garkov@uni-konstanz.de>
2018-02-16 23:43:15 +0100

    Bugfixes regarding the 'Find and Layout Circles' algorithm

--- src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/algorithms/circle_search/CircleSearchAndLayoutAlgorithm.java
+++ src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/algorithms/circle_search/CircleSearchAndLayoutAlgorithm.java
@@ -30,13 +30,22 @@ import de.ipk_gatersleben.ag_nw.graffiti.services.task.BackgroundTaskHelper;
 import de.ipk_gatersleben.ag_nw.graffiti.services.task.BackgroundTaskStatusProviderSupportingExternalCallImpl;
 
 /**
+ * Searches for circular sub-graphs and optionally applies a CircleLayouter.
+ * 
+ * Modified in version 2.6.5 to improve parameter handling (Dimitar Garkov).
+ * 
  * @author Christian Klukas
+ * @version 2.6.5
  */
 public class CircleSearchAndLayoutAlgorithm extends AbstractAlgorithm {
 	
+	/** The minimum number of nodes that can form circle.*/
+	public static final Integer MIN_CIRCLE_SIZE = 3;
+	
+	private int startNodeCount;
+	private int endNodeCount;
+	private int currentGraphID;
 	private double patternNodeDistance = 50;
-	private int startNodeCount = 10;
-	private int endNodeCount = 3;
 	private boolean doCircleLayout = true;
 	private boolean startWithLargestCircle = true;
 	
@@ -45,31 +54,40 @@ public class CircleSearchAndLayoutAlgorithm extends AbstractAlgorithm {
 		return "Layout";
 	}
 	
-	
-	
 	@Override
 	public boolean isLayoutAlgorithm() {
 		return true;
 	}
-
-
-
+	/**
+	 * @return Find and Layout Circles
+	 */
+	public String getName() {
+		return "Find and Layout Circles";
+	}
+	
 	@Override
 	public String getDescription() {
 		return "<html>" +
-							"This algorithm searches for circular sub-networks<br>" +
-							"and applies a circle layout to these sub-networks.<br>" +
-							"Plase specify the aproximate node distance and<br>" +
-							"the minimum and maximum size of circles (node count).<br>" +
-							"<small>Remark: There may be additional, overlapping circles -<br>" +
-							"only one of the possibilities is processed.</small>";
+						"This algorithm searches for circular sub-networks<br>" +
+						"and applies a circle layout to these sub-networks.<br>" +
+						"Please, specify the aproximate node distance and<br>" +
+						"the minimum and maximum size of circles (node count).<br>" +
+						"<small>Remark: There may be additional, overlapping circles -<br>" +
+						"only one of the possibilities is processed.</small>";
 	}
 	
 	@Override
 	public Parameter[] getParameters() {
+		int nNodes = graph.getNumberOfNodes();
+		
+		//lazy reset (only when necessary)
+		if (endNodeCount > startNodeCount)
+			endNodeCount = MIN_CIRCLE_SIZE;
+		
 		return new Parameter[] {
-							new IntegerParameter(10, 3, graph.getNumberOfNodes(), "Maximum Node Count", ""),
-							new IntegerParameter(3, 3, graph.getNumberOfNodes(), "Minimum Node Count", ""),
+							new IntegerParameter((startNodeCount > nNodes) ? nNodes : startNodeCount,
+									MIN_CIRCLE_SIZE, nNodes, "Maximum Node Count", ""),
+							new IntegerParameter(endNodeCount, MIN_CIRCLE_SIZE, nNodes, "Minimum Node Count", ""),
 							new BooleanParameter(startWithLargestCircle, "Start search with large circles",
 												"If enabled, the circle search starts with the maximum node count."),
 							new BooleanParameter(doCircleLayout, "Apply Circle Layout/Select Nodes",
@@ -90,10 +108,17 @@ public class CircleSearchAndLayoutAlgorithm extends AbstractAlgorithm {
 	
 	@Override
 	public void check() throws PreconditionException {
-		if (graph == null || graph.getNumberOfNodes() < 3)
-			throw new PreconditionException("No active network or network too small (less than 3 nodes)!");
-		if (startNodeCount < endNodeCount)
-			throw new PreconditionException("Start node count can not be smaller than end node count!");
+		if (graph == null)
+			throw new PreconditionException("No active network!");
+		if (graph.getNumberOfNodes() < MIN_CIRCLE_SIZE)
+			throw new PreconditionException("Network too small (less than 3 nodes)!");
+		
+		//first or new graph -> set counts
+		if (currentGraphID != graph.hashCode()) {
+			startNodeCount = graph.getNumberOfNodes();
+			endNodeCount = MIN_CIRCLE_SIZE;
+			currentGraphID = graph.hashCode();
+		}			
 	}
 	
 	public void execute() {
@@ -103,24 +128,29 @@ public class CircleSearchAndLayoutAlgorithm extends AbstractAlgorithm {
 		final BackgroundTaskStatusProviderSupportingExternalCallImpl status = new BackgroundTaskStatusProviderSupportingExternalCallImpl("", "");
 		final Graph graph = this.graph;
 		final boolean startLarge = startWithLargestCircle;
-		BackgroundTaskHelper.issueSimpleTask("Find and layout circles", "Please wait...",
+		BackgroundTaskHelper.issueSimpleTask(this.getName(), "Please wait...",
 							new Runnable() {
 								public void run() {
 									List<Graph> circleGraphs = new LinkedList<Graph>();
 									for (int n = startNodeCount; n >= endNodeCount; n--) {
 										if (status.wantsToStop())
 											break;
+										
 										status.setCurrentStatusText2("Create circle of size " + n);
 										Graph circleGraph = new AdjListGraph();
 										Node previousNode = null;
 										Node firstNode = null;
+										
 										for (int i = 0; i < n; i++) {
+											if (status.wantsToStop())
+												break;
+											
 											Node node = circleGraph.addNode(AttributeHelper.getDefaultGraphicsAttributeForNode(20, 20));
 											if (firstNode == null)
 												firstNode = node;
-											if (previousNode != null) {
+											if (previousNode != null)
 												circleGraph.addEdge(previousNode, node, true);
-											}
+
 											previousNode = node;
 										}
 										if (firstNode != null && previousNode != null)
@@ -159,10 +189,6 @@ public class CircleSearchAndLayoutAlgorithm extends AbstractAlgorithm {
 							null, status);
 	}
 	
-	public String getName() {
-		return "Find and Layout Circles";
-	}
-	
 	@Override
 	public Set<Category> getSetCategory() {
 		return new HashSet<Category>(Arrays.asList(
@@ -172,10 +198,8 @@ public class CircleSearchAndLayoutAlgorithm extends AbstractAlgorithm {
 				));
 	}
 
-	
 	@Override
 	public boolean mayWorkOnMultipleGraphs() {
 		return true;
 	}
-	
 }
