62752c42fd7109e977f9895b79035cccdb20b4ad
Jannik Löscher <polymehr.service@gmail.com>
2019-05-23 20:59:12 +0200

    sm: 11: Create tests, fix bugs and optimize
    + Add test cases for 'NodeValueMatrix'
    * Fixed flipped unequal signs
    * Change thrown except type when trying to set diagonal
    * Optimize imports
    * Small documentation changes
    - Remove `throws CloneNotSupportedException` of 'clone()' in 'NodeValueMatrix'

--- mlf/vanted-addon-sm/src/main/java/org/vanted/addons/stressminaddon/util/NodeValueMatrix.java
+++ mlf/vanted-addon-sm/src/main/java/org/vanted/addons/stressminaddon/util/NodeValueMatrix.java
@@ -1,10 +1,6 @@
 package org.vanted.addons.stressminaddon.util;
 
-import org.graffiti.graph.Node;
-
 import java.util.Arrays;
-import java.util.IdentityHashMap;
-import java.util.Map;
 import java.util.function.DoubleUnaryOperator;
 
 /**
@@ -36,7 +32,7 @@ public class NodeValueMatrix implements Cloneable {
     public NodeValueMatrix(final int dimension) {
         if ((this.dimension = dimension) <= 0)
             throw new IllegalArgumentException("Dimension must be positive!");
-        // initialize the values (internally only a (d-1)*(d-1) half matrix is saved
+        // initialize the values (internally only a (d-1)*(d-1) triangular matrix is saved
         this.values = new double[dimension-1][];; // the first row is always empty
         for (int row = 0; row < dimension - 1; row++) {
             this.values[row] = new double[row+1];
@@ -81,7 +77,7 @@ public class NodeValueMatrix implements Cloneable {
             throw new IndexOutOfBoundsException("Index of 'col' out of bounds: 0 <= col <= " + (dimension-1));
         // get the value
         if (row == col)     return 0; // the diagonal is always 0
-        else if (row < col) return this.values[row-1][col]; // the first row is not saved
+        else if (row > col) return this.values[row-1][col]; // the first row is not saved
         else                return this.values[col-1][row]; // the matrix is symmetrical
     }
 
@@ -93,7 +89,7 @@ public class NodeValueMatrix implements Cloneable {
      * @param value the value to set.
      *
      * @throws IndexOutOfBoundsException if {@code row} or {@code col} are out of bounds.
-     * @throws IllegalArgumentException if {@code row} and {@code col} are equal.
+     * @throws UnsupportedOperationException if {@code row} and {@code col} are equal.
      */
     public void set(final int row, final int col, final double value) {
         if (row < 0 || row > dimension-1)
@@ -101,9 +97,9 @@ public class NodeValueMatrix implements Cloneable {
         if (col < 0 || col > dimension-1)
             throw new IndexOutOfBoundsException("Index 'col' out of bounds: 0 <= col <= " + (dimension-1));
         if (row == col)
-            throw new IllegalArgumentException("The diagonal may not be set (row==col)");
+            throw new UnsupportedOperationException("The diagonal may not be set (row==col)");
         // set the value
-        else if (row < col) this.values[row-1][col] = value; // the first row is not saved
+        else if (row > col) this.values[row-1][col] = value; // the first row is not saved
         else                this.values[col-1][row] = value; // the matrix is symmetrical
     }
 
@@ -120,7 +116,7 @@ public class NodeValueMatrix implements Cloneable {
     public NodeValueMatrix apply(final DoubleUnaryOperator operator) {
         if (null == operator) throw new NullPointerException("Operator may not be null!");
         for (int row = 0; row < dimension - 1; row++) {
-            for (int col = 0; col < dimension - 1; col++) {
+            for (int col = 0; col < row + 1; col++) {
                 this.values[row][col] = operator.applyAsDouble(this.values[row][col]);
             }
         }
@@ -128,7 +124,7 @@ public class NodeValueMatrix implements Cloneable {
     }
 
     @Override
-    protected NodeValueMatrix clone() throws CloneNotSupportedException {
+    protected NodeValueMatrix clone() {
         return new NodeValueMatrix(this);
     }
 }
