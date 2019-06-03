ef9392459c4cb480123feee23eb0939fd0d6e1c0
Gordian Schönherr <gordian.schoenherr@uni-konstanz.de>
2019-06-03 21:20:58 +0200

    add a simple demo GUI for the MLF
    * Note that this commit doesn't compile because the RandomMerger
      and the RandomPlacer are not contained here
      (they are being worked on on other branches at the moment)

--- mlf/vanted-addon-mlf/src/main/java/org/vanted/addons/multilevelframework/MergedNode.java
+++ mlf/vanted-addon-mlf/src/main/java/org/vanted/addons/multilevelframework/MergedNode.java
@@ -1,11 +1,15 @@
 package org.vanted.addons.multilevelframework;
 
+import de.ipk_gatersleben.ag_nw.graffiti.NodeTools;
 import org.AttributeHelper;
+import org.Vector2d;
 import org.graffiti.attributes.CollectionAttribute;
 import org.graffiti.graph.AdjListNode;
+import org.graffiti.graph.Edge;
 import org.graffiti.graph.Graph;
 import org.graffiti.graph.Node;
 
+import java.lang.reflect.Field;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,6 +17,7 @@ import java.util.stream.Collectors;
 
 /**
  * A {@link Node} implementation that represents several nodes in a coarsening level within {@link MultilevelGraph}.
+ *
  * @see org.graffiti.graph.AdjListNode
  * @see Node
  */
@@ -21,75 +26,105 @@ public class MergedNode extends AdjListNode {
 
     /**
      * Create a new {@link MergedNode}.
-     * @param g
-     *     The {@link Graph} that contains this node. Must not be {@code null}.
-     * @param nodes
-     *     The {@link Node}s represented by this node. Must not be {@code null}.
-     *     Note that the collection will not be copied, but is used directly by this instance.
-     * @see AdjListNode#AdjListNode(Graph)
+     *
+     * @param g     The {@link Graph} that contains this node. Must not be {@code null}.
+     * @param nodes The {@link Node}s represented by this node. Must not be {@code null}.
+     *              Note that the collection will not be copied, but is used directly by this instance.
      * @author Gordian
+     * @see AdjListNode#AdjListNode(Graph)
      */
     public MergedNode(Graph g, Set<Node> nodes) {
         super(Objects.requireNonNull(g, "MergedNode graph must not be null."));
         this.nodes = Objects.requireNonNull(nodes, "MergedNode nodes must not be null.");
         this.updateLabel();
+        this.updatePosition();
     }
 
     /**
-     * Create a new {@link MergedNode}.
-     * @param g
-     *     The {@link Graph} that contains this node. Must not be {@code null}.
-     * @see AdjListNode#AdjListNode(Graph, CollectionAttribute)
+     * Create a new {@link MergedNode}. It is preferable to use the other constructor and refrain from using
+     * {@link MergedNode#addInnerNode(Node)}.
+     *
+     * @param g The {@link Graph} that contains this node. Must not be {@code null}.
      * @author Gordian
+     * @see AdjListNode#AdjListNode(Graph, CollectionAttribute)
      */
+    @Deprecated
     public MergedNode(Graph g, CollectionAttribute col) {
         super(Objects.requireNonNull(g), Objects.requireNonNull(col));
         this.nodes = new HashSet<>();
         this.updateLabel();
+        this.updatePosition();
     }
 
     /**
-     * Create a new {@link MergedNode}.
-     * @param g
-     *     The {@link Graph} that contains this node. Must not be {@code null}.
-     * @see AdjListNode#AdjListNode(Graph)
+     * Create a new {@link MergedNode}. It is preferable to use the other constructor and refrain from using
+     * {@link MergedNode#addInnerNode(Node)}.
+     *
+     * @param g The {@link Graph} that contains this node. Must not be {@code null}.
      * @author Gordian
+     * @see AdjListNode#AdjListNode(Graph)
      */
+    @Deprecated
     public MergedNode(Graph g) {
         super(g);
         this.nodes = new HashSet<>();
         this.updateLabel();
+        this.updatePosition();
     }
 
     /**
-     * @return
-     *    the {@link Node}s represented by this {@link MergedNode}.
-     *    The returned value must not be modified (see {@link MergedNode#addInnerNode(Node)}).
+     * @return the {@link Node}s represented by this {@link MergedNode}.
+     * The returned value must not be modified (see {@link MergedNode#addInnerNode(Node)}).
      * @author Gordian
      */
-    public Collection<?extends Node> getInnerNodes() {
+    public Collection<? extends Node> getInnerNodes() {
         return Collections.unmodifiableCollection(this.nodes);
     }
 
     /**
      * Use this method to add a {@link Node} to the collection of nodes represented by this
      * {@link MergedNode}.
-     * @param node
-     *     The {@link Node} to be added. Must not be {@code null}. Must not be already contained in this
-     *     {@link MergedNode} (i.e. not in {@link MergedNode#getInnerNodes()}).
+     * It is better to add all inner nodes using the constructor {@link MergedNode#MergedNode(Graph, Set)},
+     * because otherwise the position has to be recalculated on each change.
+     *
+     * @param node The {@link Node} to be added. Must not be {@code null}. Must not be already contained in this
+     *             {@link MergedNode} (i.e. not in {@link MergedNode#getInnerNodes()}).
      * @author Gordian
      */
+    @Deprecated
     public void addInnerNode(Node node) {
         if (this.nodes.contains(node)) {
             throw new IllegalArgumentException("MergedNode must not contain the same node twice.");
         }
         this.nodes.add(Objects.requireNonNull(node, "MergedNode cannot represent null nodes."));
         this.updateLabel();
+        this.updatePosition();
+    }
+
+    /**
+     * Set the position of this {@link MergedNode} to the center of the represented nodes
+     * (i.e. the value returned by {@link NodeTools#getCenter(Collection)})
+     * In case the returned center contains {@code NaN} values (see {@link Double#isNaN(double)}) or there are no
+     * represented nodes the center will be set to the origin (0,0).
+     *
+     * @author Gordian
+     */
+    private void updatePosition() {
+        if (this.nodes.isEmpty()) {
+            AttributeHelper.setPosition(this, 0, 0);
+            return;
+        }
+        Vector2d center = NodeTools.getCenter(this.getInnerNodes());
+        if (Double.isNaN(center.x) || Double.isNaN(center.y)) {
+            center = new Vector2d(0, 0);
+        }
+        AttributeHelper.setPosition(this, center);
     }
 
     /**
      * Create a label that contains information about the nodes represented by this {@link MergedNode}.
      * The label is set using {@link AttributeHelper}.
+     *
      * @author Gordian, Tobias
      */
     private void updateLabel() {
