92574453cdae3c6b4462dcd1752556e5cd9c17cd
Gordian Schönherr <gordian.schoenherr@uni-konstanz.de>
2019-06-05 18:53:16 +0200

    MultilevelFrameworkLayouter: fix GUI not updating, fix undo
    * the GUI now updates the algorithm list if new algorithms are added
      (e.g. if the user installs a new add-on)
    * the layout is now properly undoable

--- mlf/vanted-addon-mlf/src/main/java/org/vanted/addons/multilevelframework/MultilevelFrameworkLayouter.java
+++ mlf/vanted-addon-mlf/src/main/java/org/vanted/addons/multilevelframework/MultilevelFrameworkLayouter.java
@@ -15,22 +15,18 @@ import de.ipk_gatersleben.ag_nw.graffiti.GraphHelper;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.layouters.connected_components.ConnectedComponentLayout;
 import org.AttributeHelper;
 import org.Vector2d;
-import org.graffiti.attributes.Attribute;
 import org.graffiti.editor.MainFrame;
 import org.graffiti.graph.Node;
 import org.graffiti.plugin.algorithm.*;
 import org.graffiti.plugin.parameter.JComponentParameter;
-import org.graffiti.plugin.parameter.ObjectListParameter;
 import org.graffiti.plugin.parameter.Parameter;
 import org.graffiti.plugin.view.View;
 import org.graffiti.selection.Selection;
+import org.graffiti.session.Session;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.*;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
 
@@ -38,6 +34,7 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
     private final static String DEFAULT_ALGORITHM = "Force Directed (\"parameter\" GUI)";
     private JComboBox<String> algorithmListComboBox;
     private JButton setUpLayoutAlgorithmButton;
+    private String lastSelectedAlgorithm = DEFAULT_ALGORITHM;
 
     public MultilevelFrameworkLayouter() {
         super();
@@ -50,11 +47,14 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
     }
 
     /**
+     * Does nothing. This layouter doesn't currently support resetting; it causes problems with the GUI of
+     * sub-algorithms (such as the level layouter).
      * @see super#reset()
      */
     @Override
     public void reset() {
         super.reset();
+        this.updateParameters();
     }
 
     /**
@@ -84,6 +84,10 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
      * Performs the layout.
      */
     public void execute() {
+        // need to save old session and view, because they need to be restored for applyUndoableNodePositionUpdate
+        // to work
+        final Session oldSession = MainFrame.getInstance().getActiveSession();
+        final View oldView = MainFrame.getInstance().getActiveSession().getActiveView();
         // split the subgraph induced by the selection into connected components
         final Collection<? extends CoarsenedGraph> connectedComponents =
                 MlfHelper.calculateConnectedComponentsOfSelection(new HashSet<>(this.getSelectedOrAllNodes()));
@@ -100,6 +104,7 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
             merger.buildCoarseningLevels(componentMLG);
             while (componentMLG.getNumberOfLevels() > 1) {
                 System.out.println("Layouting level " + componentMLG.getNumberOfLevels());
+                GraphHelper.diplayGraph(componentMLG.getTopLevel());
                 algorithm.execute(componentMLG.getTopLevel(), emptySelection);
                 placer.reduceCoarseningLevel(componentMLG);
             }
@@ -116,10 +121,12 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
             for (MergedNode mn : cg.getMergedNodes()) {
                 final Node representedNode = mn.getInnerNodes().iterator().next();
                 assert mn.getInnerNodes().size() == 1 : "More than one node represented in level 0";
-                AttributeHelper.setPosition(representedNode, AttributeHelper.getPosition(mn));
+                nodes2newPositions.put(representedNode, AttributeHelper.getPositionVec2d(mn));
             }
         }
 
+
+        MainFrame.getInstance().setActiveSession(oldSession, oldView);
         GraphHelper.applyUndoableNodePositionUpdate(nodes2newPositions, getName());
         ConnectedComponentLayout.layoutConnectedComponents(this.graph);
     }
@@ -129,13 +136,14 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
      */
     @Override
     public Parameter[] getParameters() {
+        this.updateParameters();
         return this.parameters;
     }
 
     /**
-     * Sets the radius parameter to the given value.
-     *
-     * @param params An array with exact one DoubleParameter.
+     * Sets the parameters to the given array.
+     * @param params The new parameters.
+     * @see Algorithm#setParameters(Parameter[])
      */
     @Override
     public void setParameters(Parameter[] params) {
@@ -152,6 +160,7 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
 
     /**
      * This makes sure the algorithm is moved to the layout-tab of VANTED
+     * @return {@code true}
      */
     @Override
     public boolean isLayoutAlgorithm() {
@@ -169,12 +178,26 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
      * @author Gordian
      */
     private void clickSetUpLayoutAlgorithmButton(ActionEvent e) {
+        final LayoutAlgorithmWrapper current = this.layoutAlgorithms.get(this.lastSelectedAlgorithm);
+        final JComponent gui = current.getGUI();
+        if (gui != null) {
+            JOptionPane.showMessageDialog(MainFrame.getInstance(), gui,
+                    this.lastSelectedAlgorithm, JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
+    /**
+     * The handler that gets called if the user changes to a different layout algorithm for the levels.
+     * @param e
+     *      Ignored.
+     * @author Gordian
+     */
+    private void changeSelectedAlgorithm(ActionEvent e) {
         final String selected = (String) this.algorithmListComboBox.getSelectedItem();
         if (selected == null) {
             return;
         }
-        final LayoutAlgorithmWrapper current = this.layoutAlgorithms.get(selected);
-        JOptionPane.showMessageDialog(MainFrame.getInstance(), current.getGUI(), selected, JOptionPane.PLAIN_MESSAGE);
+        this.lastSelectedAlgorithm = selected;
     }
 
     /**
@@ -182,23 +205,45 @@ public class MultilevelFrameworkLayouter extends AbstractEditorAlgorithm {
      * @author Gordian
      */
     private void setUpParameters() {
-        this.layoutAlgorithms = LayoutAlgorithmWrapper.getLayoutAlgorithms();
-        String defaultAlgorithm = this.layoutAlgorithms.keySet().contains(DEFAULT_ALGORITHM) ? DEFAULT_ALGORITHM :
-                this.layoutAlgorithms.keySet().iterator().next();
-        this.algorithmListComboBox = new JComboBox<>(this.layoutAlgorithms.keySet().stream()
-                .sorted().toArray(String[]::new));
-        JComponentParameter algorithmList = new JComponentParameter(this.algorithmListComboBox, "Layout Algorithm",
-                "Layout Algorithm to be run on each level of the coarsened graph.");
-        this.algorithmListComboBox.setSelectedItem(defaultAlgorithm);
+        this.layoutAlgorithms = new HashMap<>();
+        this.updateParameters();
+    }
 
-        this.setUpLayoutAlgorithmButton = new JButton("Set up layouter");
-        this.setUpLayoutAlgorithmButton.addActionListener(this::clickSetUpLayoutAlgorithmButton);
-        JComponentParameter setUpLayoutAlgorithmButtonParameter =
-                new JComponentParameter(this.setUpLayoutAlgorithmButton, "Set up layouter",
-                        "Click the button to change the parameters of the layout algorithm.");
+    private void updateParameters() {
+        Map<String, LayoutAlgorithmWrapper> tmp = LayoutAlgorithmWrapper.getLayoutAlgorithms();
+        boolean changed = false;
+        for (Map.Entry<String, LayoutAlgorithmWrapper> e : tmp.entrySet()) {
+            if (!this.layoutAlgorithms.containsKey(e.getKey())) {
+                this.layoutAlgorithms.put(e.getKey(), e.getValue());
+                changed = true;
+            }
+        }
+        if (changed || this.parameters == null) {
+            if (!this.layoutAlgorithms.containsKey(this.lastSelectedAlgorithm)) {
+                MainFrame.showMessageDialog("Algorithm \"" + this.layoutAlgorithms + "\" doesn't seem to exist.",
+                        "Error");
+                this.lastSelectedAlgorithm = this.layoutAlgorithms.keySet().contains(this.lastSelectedAlgorithm) ?
+                        this.lastSelectedAlgorithm : this.layoutAlgorithms.keySet().iterator().next();
+            }
 
-        // TODO add GUI for Mergers and Placers
+            this.algorithmListComboBox = new JComboBox<>(this.layoutAlgorithms.keySet().stream()
+                    .sorted().toArray(String[]::new));
+            this.algorithmListComboBox.addActionListener(this::changeSelectedAlgorithm);
 
-        this.parameters = new Parameter[] {algorithmList, setUpLayoutAlgorithmButtonParameter};
+            JComponentParameter algorithmList = new JComponentParameter(this.algorithmListComboBox, "Layout Algorithm",
+                    "Layout Algorithm to be run on each level of the coarsened graph.");
+            this.algorithmListComboBox.setSelectedItem(this.lastSelectedAlgorithm);
+
+            this.setUpLayoutAlgorithmButton = new JButton("Set up layouter");
+            this.setUpLayoutAlgorithmButton.addActionListener(this::clickSetUpLayoutAlgorithmButton);
+
+            JComponentParameter setUpLayoutAlgorithmButtonParameter =
+                    new JComponentParameter(this.setUpLayoutAlgorithmButton, "Set up layouter",
+                            "Click the button to change the parameters of the layout algorithm.");
+
+            // TODO add GUI for Mergers and Placers
+
+            this.parameters = new Parameter[]{algorithmList, setUpLayoutAlgorithmButtonParameter};
+        }
     }
 }
