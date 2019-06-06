685c5977fad364b85b1250f1191b50e755678ed8
David Boetius <david.boetius@uni-konstanz.de>
2019-06-06 15:39:40 +0200

    Added another scaling benchmark

---
+++ stressminimization/vantedsource/StressMinimizationTests/src/main/test/org/vanted/addons/stressminimization/benchmark/Pair.java
@@ -0,0 +1,12 @@
+package org.vanted.addons.stressminimization.benchmark;
+
+public class Pair<T, U> {
+	public final T first;
+	public final U second;
+	
+	public Pair(T first, U second) {
+		super();
+		this.first = first;
+		this.second = second;
+	}
+}
