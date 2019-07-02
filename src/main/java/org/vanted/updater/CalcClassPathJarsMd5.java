aaf4e27a36c23ccddcb2afcb651655250351989d
Dimitar Garkov <dimitar.garkov@uni-konstanz.de>
2019-07-02 15:29:50 +0200

    Complex attribute paths can now be added using the AttributeHelper
    Bugfix jar loading on update check

--- src/main/java/org/vanted/updater/CalcClassPathJarsMd5.java
+++ src/main/java/org/vanted/updater/CalcClassPathJarsMd5.java
@@ -1,11 +1,8 @@
 package org.vanted.updater;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -42,29 +39,20 @@ public class CalcClassPathJarsMd5 {
 	 */
 	public static List<Pair<String, String>> getJarMd5Pairs() {
 		List<Pair<String, String>> listJarMd5Pairs = new ArrayList<Pair<String, String>>();
-
 		try {
 			MessageDigest.getInstance("MD5");
-
-			if (!(CalcClassPathJarsMd5.class.getClassLoader() instanceof URLClassLoader)) {
-				ErrorMsg.addErrorMessage("Class loader is not an URLClassLoader. No update scan!");
-				return listJarMd5Pairs;
-			}
-
-			URLClassLoader cl = (URLClassLoader) CalcClassPathJarsMd5.class.getClassLoader();
-			for (URL jarurl : cl.getURLs()) {
-				Path jarpath;
-				try (InputStream is = Files.newInputStream(jarpath = Paths.get(jarurl.toURI()),
+			Path jarpath;
+			// Skip vanted/target/classes directory
+			final String omitPath = "target" + File.separatorChar + "classes";
+			for (String jarString : getClasspathEntries()) {				
+				if (jarString.endsWith(omitPath))
+					continue;
+				
+				try (InputStream is = Files.newInputStream(jarpath = Paths.get(jarString),
 						StandardOpenOption.READ)) {
 					String md5 = DigestUtils.md5Hex(Files.readAllBytes(jarpath));
-					listJarMd5Pairs.add(new Pair<String, String>(jarpath.toString(), md5));
-				} catch (AccessDeniedException ade) {
-					// This exception happens only on Windows from Java 8 and not in production
-					// (on /target/classes). Therefore, report only in console.
-					ade.printStackTrace();
-				} catch (SecurityException | URISyntaxException e) {
-					ErrorMsg.addErrorMessage(e);
-				} catch (IOException e) {
+					listJarMd5Pairs.add(new Pair<String, String>(jarString, md5));
+				} catch (SecurityException | IOException e) {
 					ErrorMsg.addErrorMessage(e);
 				}
 			}
@@ -75,4 +63,12 @@ public class CalcClassPathJarsMd5 {
 		return listJarMd5Pairs;
 	}
 
+	/**
+	 * 
+	 * @return the paths of the JARs in the classpath as strings
+	 * @since 2.7.0
+	 */
+	public static String[] getClasspathEntries() {
+		return System.getProperty("java.class.path").split(System.getProperty("path.separator"));
+	}
 }
