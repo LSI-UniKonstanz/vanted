9065536dfe3b295f7561a6a6a886c81388565b75
Dimitar Garkov <dimitar.garkov@uni-konstanz.de>
2020-07-24 15:50:22 +0200

    Handle non-parsable kgml strings

--- src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/ios/kgml/datatypes/kgmlNumber.java
+++ src/main/java/de/ipk_gatersleben/ag_nw/graffiti/plugins/ios/kgml/datatypes/kgmlNumber.java
@@ -21,16 +21,24 @@ public class kgmlNumber {
 		this.value = value;
 	}
 
+	/**
+	 * Parses a string to a kgml number.
+	 * @param s string number
+	 * @return Null, if the string cannot be parsed to an integer.
+	 * @vanted.revision 2.7.3
+	 */
 	public static kgmlNumber getNumber(String s) {
+		Thread.dumpStack();
 		try {
-			return new kgmlNumber(Integer.parseInt(s));
+			if (s != null && s.matches("[+-]?\\d+"))
+				return new kgmlNumber(Integer.parseInt(s));
 		} catch (NumberFormatException nfe) {
 			System.out.println("Number format exception: " + s);
-			return null;
 		} catch (Exception e) {
-			System.out.println("NN: " + s);
-			return null;
+			System.out.println("Exception at kgmlNumber.getNumber(): " + s);
 		}
+		
+		return null;
 	}
 
 	@Override
