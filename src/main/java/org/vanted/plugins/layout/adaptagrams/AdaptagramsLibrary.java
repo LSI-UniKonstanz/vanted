7d30961a1470c575b1adf358519e0868f3f0b3f7
niklas-groene <95590600+niklas-groene@users.noreply.github.com>
2024-12-01 19:53:53 +0100

    Update AdaptagramsLibrary.java

--- src/main/java/org/vanted/plugins/layout/adaptagrams/AdaptagramsLibrary.java
+++ src/main/java/org/vanted/plugins/layout/adaptagrams/AdaptagramsLibrary.java
@@ -91,7 +91,7 @@ public class AdaptagramsLibrary {
 		}
 		// on Mac OS only one fat native library exists for each architecture type
 		if (osName.toLowerCase().contains(availableOSs[2])){
-			ext = oldMacExt;
+			ext = macExt;
 		}
 		
 		// check whether the library can be found
